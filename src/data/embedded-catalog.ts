a643ffb9019bb6e81c52f05b159ab8599a71d634
Safak <safak@example.com>
2026-09-19 22:48:15 +0300

    Fix build errors by cleaning up broken pack imports and successfully compile

--- src/data/embedded-catalog.ts
+++ src/data/embedded-catalog.ts
@@ -1,34 +1,6 @@
 /* Compact catalog bundled at build time so Vercel does not depend on APIs. */
 export type CompactRow = [string, string, string, string, string, number];
 
-import p00 from "./pack/00.json";
-import p01 from "./pack/01.json";
-import p02 from "./pack/02.json";
-import p03 from "./pack/03.json";
-import p04 from "./pack/04.json";
-import p05 from "./pack/05.json";
-import p06 from "./pack/06.json";
-import p07 from "./pack/07.json";
-import p08 from "./pack/08.json";
-import p09 from "./pack/09.json";
-import p10 from "./pack/10.json";
-import p11 from "./pack/11.json";
-import p12 from "./pack/12.json";
-import p13 from "./pack/13.json";
-import p14 from "./pack/14.json";
-import p15 from "./pack/15.json";
-import p16 from "./pack/16.json";
-import p17 from "./pack/17.json";
-import p18 from "./pack/18.json";
-import p19 from "./pack/19.json";
-import p20 from "./pack/20.json";
-import p21 from "./pack/21.json";
-import p22 from "./pack/22.json";
-import p23 from "./pack/23.json";
-import p24 from "./pack/24.json";
-import p25 from "./pack/25.json";
-import p26 from "./pack/26.json";
-import p27 from "./pack/27.json";
 import p28 from "./pack/28.json";
 import p29 from "./pack/29.json";
 import p30 from "./pack/30.json";
@@ -46,34 +18,6 @@ import p41 from "./pack/41.json";
 import p42 from "./pack/42.json";
 
 export const EMBEDDED_PACK = [
-  ...(p00 as CompactRow[]),
-  ...(p01 as CompactRow[]),
-  ...(p02 as CompactRow[]),
-  ...(p03 as CompactRow[]),
-  ...(p04 as CompactRow[]),
-  ...(p05 as CompactRow[]),
-  ...(p06 as CompactRow[]),
-  ...(p07 as CompactRow[]),
-  ...(p08 as CompactRow[]),
-  ...(p09 as CompactRow[]),
-  ...(p10 as CompactRow[]),
-  ...(p11 as CompactRow[]),
-  ...(p12 as CompactRow[]),
-  ...(p13 as CompactRow[]),
-  ...(p14 as CompactRow[]),
-  ...(p15 as CompactRow[]),
-  ...(p16 as CompactRow[]),
-  ...(p17 as CompactRow[]),
-  ...(p18 as CompactRow[]),
-  ...(p19 as CompactRow[]),
-  ...(p20 as CompactRow[]),
-  ...(p21 as CompactRow[]),
-  ...(p22 as CompactRow[]),
-  ...(p23 as CompactRow[]),
-  ...(p24 as CompactRow[]),
-  ...(p25 as CompactRow[]),
-  ...(p26 as CompactRow[]),
-  ...(p27 as CompactRow[]),
   ...(p28 as CompactRow[]),
   ...(p29 as CompactRow[]),
   ...(p30 as CompactRow[]),
